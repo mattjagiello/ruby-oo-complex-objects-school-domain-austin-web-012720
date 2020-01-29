d2dabe675628313b3588013e6caf76ec68b1dd3d
Matthew Jagiello <mjagiello@gmail.com>
2020-01-29 06:29:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,4 +12,11 @@ class School
       @roster[grade] << name
     end
   end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.each do |grade, name|
+      name.sort
+    end
 end
